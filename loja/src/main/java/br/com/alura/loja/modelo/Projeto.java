7aca4a92456b1ec57ba0a79b78909567180413b1
tc.vfreitas <tc.vfreitas@padtec.com.br>
2018-09-24 15:00:22 -0300

    rest

--- loja/src/main/java/br/com/alura/loja/modelo/Projeto.java
+++ loja/src/main/java/br/com/alura/loja/modelo/Projeto.java
@@ -13,10 +13,45 @@ public class Projeto {
   private int anoDeInicio;
 
 
+  public Projeto() {
+    super();
+  }
+
+
   public Projeto(String nome, long id, int anoDeInicio) {
     super();
     this.nome = nome;
     this.id = id;
+    this.anoDeInicio = anoDeInicio;
+  }
+
+
+  public String getNome() {
+    return nome;
+  }
+
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+
+  public long getId() {
+    return id;
+  }
+
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+
+  public int getAnoDeInicio() {
+    return anoDeInicio;
+  }
+
+
+  public void setAnoDeInicio(int anoDeInicio) {
     this.anoDeInicio = anoDeInicio;
   }
 
