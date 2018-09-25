87dd6fb77e20454d24845e2f17c3c9c4dfe0b5ff
tc.vfreitas <tc.vfreitas@padtec.com.br>
2018-09-25 09:02:48 -0300

    rest

--- loja/src/test/java/br/com/alura/loja/CLientTest.java
+++ loja/src/test/java/br/com/alura/loja/CLientTest.java
@@ -4,18 +4,35 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import junit.framework.Assert;
+import com.thoughtworks.xstream.XStream;
+
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.servidor.Servidor;
 
 public class CLientTest {
 
-	@Test
-	public void testaQueAConexãoComOServidorFunciona() {
+  private HttpServer server;
 
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://www.mocky.io");
-		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
-		Assert.assertTrue(conteudo.contains("<rua>Rua Vergueiro 3185"));
+	  @Before
+	  public void startaServidor() {
+	    this.server = Servidor.inicializaServidor();
+	  }
+	  @After
+    public void mataServidor() {
+        server.stop();
+    }
+	  @Test
+	  public void testaQueAConexãoComOServidorFunciona() {
+	  Client client = ClientBuilder.newClient();
+    WebTarget target = client.target("http://localhost:8080");
+    String conteudo = target.path("/carrinhos").request().get(String.class);
+    Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+    Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
 	}
 }
